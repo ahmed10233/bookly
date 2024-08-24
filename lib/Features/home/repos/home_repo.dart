222f95796ecafcfe5dd97e43aa7c5f9e5edd46ed
ahmed10233 <111250991+ahmed10233@users.noreply.github.com>
2024-08-25 00:46:07 +0300

    create home repo and it's function and failure class

---
+++ lib/Features/home/repos/home_repo.dart
@@ -0,0 +1,7 @@
+import 'package:bookly_app/Features/home/data/models/all_books_model/all_books_model.dart';
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo{
+  Future<Either<Failure,List<AllBooksModel>>> fetchAllBooks();
+}
