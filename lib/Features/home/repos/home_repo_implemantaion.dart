51c14fa7dd711c75b519a45b1652e9db7b336441
ahmed10233 <111250991+ahmed10233@users.noreply.github.com>
2024-08-25 02:50:03 +0300

    implement fetch all books function

--- lib/Features/home/repos/home_repo_implemantaion.dart
+++ lib/Features/home/repos/home_repo_implemantaion.dart
@@ -1,18 +1,32 @@
 import 'package:bookly_app/Features/home/data/models/all_books_model/all_books_model.dart';
 import 'package:bookly_app/Features/home/repos/home_repo.dart';
 import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImplemantaion implements HomeRepo {
+  final ApiService apiService;
+  HomeRepoImplemantaion(this.apiService);
+
   @override
-  Future<Either<Failure, List<AllBooksModel>>> fetchAllBooks() {
-     
-    throw UnimplementedError();
+  Future<Either<Failure, List<AllBooksModel>>> fetchAllBooks() async {
+    try {
+      var data = await apiService.get(endPoint: 'books/ar/ar/1/50/json');
+
+      // Assuming data is a List of JSON objects, you map it to AllBooksModel
+      List<AllBooksModel> books = (data as List)
+          .map((book) => AllBooksModel.fromJson(book))
+          .toList();
+
+      return right(books);
+    } catch (e) {
+    
+      return left(ServerFailure());
+    }
   }
-  
+
   @override
   Future<Either<Failure, List<AllBooksModel>>> fetchRecentAdditions() {
-   
     throw UnimplementedError();
   }
 }
