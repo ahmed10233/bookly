51ebfba6b81b3d7f7756431349d80471633f5bae
ahmed10233 <111250991+ahmed10233@users.noreply.github.com>
2024-08-25 17:41:05 +0300

    implement all cubit function

--- lib/Features/home/presentation/manger/allbooks_cubit.dart
+++ lib/Features/home/presentation/manger/allbooks_cubit.dart
@@ -1,11 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/data/models/all_books_model/all_books_model.dart';
+import 'package:bookly_app/Features/home/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'allbooks_state.dart';
 
 class AllbooksCubit extends Cubit<AllbooksState> {
-  AllbooksCubit() : super(AllbooksInitial());
+  AllbooksCubit(this.homeRepo) : super(AllbooksInitial());
+ 
+ final HomeRepo homeRepo;
 
+ Future <void>fetchAllBooks()async{
+  emit(AllbooksLoading());
+  var result = await homeRepo.fetchAllBooks();
+  result.fold((failure){
+    emit(AllbooksFailure(failure.erroMessage));
+  }, (books){
+    emit(AllbooksSuccess(books: books));
+  });
+ }
   
 }
